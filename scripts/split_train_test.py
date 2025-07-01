bd4a9a51aeb175549d9b83a48ae91d595df9b041
cedric <demuelenaere.cedric@gmail.com>
2025-07-01 15:55:08 +0200

    add split_train_test script

--- scripts/split_train_test.py
+++ scripts/split_train_test.py
@@ -1,1 +1,8 @@
-#TODO
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+df = pd.read_csv('data/loan_data_prepared.csv')
+train, test = train_test_split(df, test_size=0.2, random_state=42)
+
+train.to_csv('data/loan_data_prepared_train.csv', index=False)
+test.to_csv('data/loan_data_prepared_test.csv', index=False)
