c944252167158de0a3686d1f17c9e19b575ed238
cedric <demuelenaere.cedric@gmail.com>
2025-07-01 15:51:21 +0200

    add coment and fix round on prepare_data script

--- scripts/prepare_data.py
+++ scripts/prepare_data.py
@@ -7,6 +7,7 @@ def standardizeColumn(col: pd.Series):
 def prepareDataset(input_csv, output_csv):
 	df = pd.read_csv(input_csv)
 
+	# ignore output column
 	target_col = "loan_status"
 	target = df[target_col]
 	df = df.drop(columns=[target_col])
@@ -14,9 +15,11 @@ def prepareDataset(input_csv, output_csv):
 	numeric_cols = df.select_dtypes(include=['int64', 'float64']).columns
 	categorical_cols = df.select_dtypes(include=['object', 'category']).columns
 
-	df[numeric_cols] = df[numeric_cols].apply(standardizeColumn)
+	# standardize numerical (minus mu divided by sigma)
+	df[numeric_cols] = df[numeric_cols].apply(standardizeColumn).round(3)
 
-	df = pd.get_dummies(df, columns=categorical_cols).astype(float).round(3)
+	# will apply one_hot encoding to categorical values
+	df = pd.get_dummies(df, columns=categorical_cols).astype(float)
 
 	df[target_col] = target
 
